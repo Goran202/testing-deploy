e7d9432b0118e47fd464d47bad3c92107eb826a8
Goran Ljaljić <gljaljic@gmail.com>
2019-09-17 09:27:24 +0200

    Updates

---
+++ precache-manifest.9c9f1034eef1decc70f4b761059134eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298b08c4131fc6dbfe3f9054a354c6e3",
+    "url": "/testing-deploy/index.html"
+  },
+  {
+    "revision": "ba5e0e1ab737ec76862f",
+    "url": "/testing-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d9a2aeee25257ec0f836",
+    "url": "/testing-deploy/static/js/2.9a115f1c.chunk.js"
+  },
+  {
+    "revision": "ba5e0e1ab737ec76862f",
+    "url": "/testing-deploy/static/js/main.7d8f33d3.chunk.js"
+  },
+  {
+    "revision": "ae099afc970eb7f24b44",
+    "url": "/testing-deploy/static/js/runtime~main.0c49b5b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
